16fd5c2dedc05753c310db6cdab5d34d8aef15aa
Broderick <asclepiius.unknown@gmail.com>
2022-04-05 22:21:37 +1000

    Attempt brute-force generation

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ def menu() -> None:
 
 def createSudoku() -> None:
     """Allows the user to create a standard 81 cell sudoku with their given number of clues filled in."""
-    s = Sudoku()
-    result = badGenerator(s, 17)
-    s.printSudoku()
+    s = Sudoku(importSudoku=True)
+    # result = badGenerator(s, 17)
+    # s.exportSudoku()
+    result = bruteForceGenerator(s, 36)
+    result.printSudoku()
 
 
 def generateSudoku() -> None:
@@ -31,9 +33,41 @@ def badGenerator(s: Sudoku(), n: int) -> Sudoku():
         n -= 1
         # Selects a cell the list of coordinates (stored as coordinate tuples)
         coord = choice(s.getEmptyCellIndices())
-        value = choice(s.getValidValues(coord))
+        value = choice(list(s.getValidValues(coord)))
         s.setCell(coord, value)
         badGenerator(s, n)
+
+
+def reduceSolution(s: Sudoku(), currentClues: int, targetClues: int = 17) -> Sudoku():
+    """Given a complete sudoku, s, this removes clues until there are targetClues left."""
+    # If this is true we have returned the system to the desired incomplete state, so we are done
+    if currentClues == targetClues:
+        return s
+    else:
+        while(True):  # Loop choosing random cells to empty until we choose one that is not empty already
+            row, col = choice(range(s.dim[0])), choice(range(s.dim[1]))
+            if s.sudoku[row][col] != s.defaultSymbol:
+                s.sudoku[row][col] = s.defaultSymbol
+                currentClues -= 1
+                return reduceSolution(s, currentClues)
+
+
+def bruteForceGenerator(s: Sudoku(), clues: int) -> Sudoku() or None:
+    """Brute-force recursive generator. Creates a sudoku, s, with clues through use of random selections and brute-force backtracking. Returns a Sudoku when a solution is found, otherwise returns None."""
+    # If true, we have solved the system and need to return the result
+    if clues == s.dim[0]*s.dim[1]:
+        return s
+    for row in range(s.dim[0]):
+        for col in range(s.dim[1]):
+            # If the cell doesn't hold the defaultSymbol, then it is already used so we should skip the cell
+            if s.sudoku[row][col] != s.defaultSymbol:
+                continue
+            for val in list(s.getValidValues((row, col))):
+                s.sudoku[row][col] = val
+                result = bruteForceGenerator(s, clues+1)
+                if type(result) == Sudoku():
+                    return result
+                s.sudoku[row][col] = s.defaultSymbol
 
 
 if __name__ == '__main__':
