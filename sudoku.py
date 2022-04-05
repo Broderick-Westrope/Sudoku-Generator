75e4ffcf23a0cb3144c232c6931b90dc4283b8a3
Broderick <asclepiius.unknown@gmail.com>
2022-04-05 21:44:23 +1000

    Add Submatrix/3x3 Check

--- sudoku.py
+++ sudoku.py
@@ -1,41 +1,35 @@
 class Sudoku:
-    def __init__(self):
+    def __init__(self, defaultSymbol: str = '?'):
+        """Initialises a new soduku with the symbol, defaultSymbol, representing empty cells."""
         # Initialises a new, blank sudoku with the standard 81 cells
-        self.sudoku = [['?' for j in range(9)]for i in range(9)]
+        self.defaultSymbol = defaultSymbol
+        self.sudoku = [[defaultSymbol for j in range(9)]for i in range(9)]
+        self.dim = self.getSudokuDimensions()
 
     def getEmptyCellIndices(self) -> list:
         """Returns a list of tuples, each of which is a coordinate for an empty cell within the sudoku."""
         coords = []
         for i in range(len(self.sudoku)-1):
             for j in range(len(self.sudoku[0])-1):
-                if self.sudoku[i][j] == '?':
+                if self.sudoku[i][j] == self.defaultSymbol:
                     coords.append((i, j))
         return coords
 
-    def getValidValues(self, coord: tuple) -> list:
+    def getValidValues(self, coord: tuple) -> set:
         """Returns a list of values (from 1 to 9 inclusive) that can legally be placed in the given cell."""
         if self.sudoku[coord[0]][coord[1]] != '':
             ValueError(
                 "Given coordinate was not empty. Cannot override a non-empty cell value.")
-        possibleValues = list(range(1, 10))
-        # Remove row and column values
-        for i in range(len(self.sudoku)-1):
-            rowValue = self.sudoku[coord[0]][i]
-            rowCount = possibleValues.count(rowValue)
-            if rowCount == 1:
-                possibleValues.remove(rowValue)
-            elif rowCount > 1:
-                ValueError("There was more than one instace of " +
-                           self.sudoku[coord[0]][i] + " in the possible values.")
-
-            colValue = self.sudoku[i][coord[1]]
-            colCount = possibleValues.count(colValue)
-            if colCount == 1:
-                possibleValues.remove(colValue)
-            elif colCount > 1:
-                ValueError("There was more than one instace of " +
-                           self.sudoku[i][coord[1]] + " in the possible values.")
-        return possibleValues
+        invalidValues = set()
+        # Add values in the same row to invalid set
+        invalidValues.add(coord[0])
+        # Add values in the same column to invalid set
+        for row in range(self.dim[1]):
+            invalidValues.add(self.sudoku[row][coord[1]])
+        # Add values in the same submatrix (3x3 group) to invalid set
+        invalidValues = invalidValues.union(self.getSubmatrixValues(coord))
+        # Perform the set difference between the potential and invalid values to produce the valid values
+        return (set(range(1, 10)) - invalidValues)
 
     def setCell(self, coord: tuple, value: int):
         """Puts the given value at the given coordinate, coord./"""
@@ -45,13 +39,21 @@ class Sudoku:
         """Returns a tuple containing the number of rows and columns that make up the sudoku."""
         return (len(self.sudoku), len(self.sudoku[0]))
 
+    def getSubmatrixValues(self, coord: tuple) -> set:
+        """Returns the values in the submatrix (3x3 cell group) of the given coordinate, coord."""
+        values = set()
+        for row in range(coord[0]//3, (coord[0]//3)+3):
+            for col in range(coord[1]//3, (coord[1]//3)+3):
+                values.add(self.sudoku[row][col])
+        return values
+
     def printSudoku(self) -> None:
         """Prints the sudoku to the CLI"""
-        dim = list(self.getSudokuDimensions())
+        dim = list(self.dim)
         # multiples of 8, minus 1
         # 8m-1
         dim[0] = int(dim[0]+(dim[0]//4))
-        val, spacer = 0, 0
+        val = spacer = 0
         for i in range(dim[0]):
             for j in range(dim[1]):
                 if (i+1)/4 == (i+1)//4 and i != dim[0]-1:
@@ -63,6 +65,7 @@ class Sudoku:
                     print(spacer, end='')
 
             print('')
+        print('')
 
 
 # 1 2 3 | 1 2 3 | 1 2 3
